f347c7d0cc1b3347cfe72ca636f124be858cfa47
AdrielStuart23 <adrielstuart9@gmail.com>
2023-01-31 12:34:42 -0400

    Calculadoras sencillas para niños

---
+++ js/multipli.js
@@ -0,0 +1,25 @@
+//variables 
+var num1 
+var num2 
+var resultado = document.getElementById("resultado")  
+//funcio del boton sumar 
+function respuestaClick () {
+   num1 = parseInt(document.getElementById("pri").value)
+   num2 = parseInt(document.getElementById("seco").value)
+    let total = num1 * num2
+    if (isNaN(total)) {
+        alert("Digite los Numeros")
+    } else(
+        resultado.innerHTML = total
+    )
+    setTimeout(comenzar, 1500)
+}
+function limpiaClick() {
+    num1 = document.getElementById("pri").value=""
+    num2 = document.getElementById("seco").value=""
+}
+function comenzar() {
+    resultado.innerHTML =""
+    
+}
+comenzar()
